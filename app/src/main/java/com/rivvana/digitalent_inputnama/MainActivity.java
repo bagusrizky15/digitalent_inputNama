b7e78de1471e9875310212c15d35f2a8df50e259
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-08-02 16:21:08 +0700

    test

--- app/src/main/java/com/rivvana/digitalent_inputnama/MainActivity.java
+++ app/src/main/java/com/rivvana/digitalent_inputnama/MainActivity.java
@@ -3,12 +3,33 @@ package com.rivvana.digitalent_inputnama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //deklarasi variabel
+
+    TextView tvNama;
+    EditText etInput;
+    Button btnTampil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNama = findViewById(R.id.tv_nama);
+        etInput = findViewById(R.id.et_input_nama);
+        btnTampil = findViewById(R.id.btn_tampilkan);
+
+        btnTampil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvNama.setText("Nama Anda : " + etInput.getText().toString() );
+            }
+        });
     }
 }
